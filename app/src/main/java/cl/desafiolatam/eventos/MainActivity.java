fc003805fc195263aa719a3c08ab982c73e62cc1
LeoCofre <leocofre1981@gmail.com>
2021-03-25 17:51:40 -0300

    Probando mostrar un WebView

---
+++ app/src/main/java/cl/desafiolatam/eventos/MainActivity.java
@@ -0,0 +1,30 @@
+package cl.desafiolatam.eventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.wvTest);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://github.com/LeoCofre");
+
+
+
+    }
+}
